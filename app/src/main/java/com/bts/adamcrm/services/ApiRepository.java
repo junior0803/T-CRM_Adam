33c85b22dfc9988193c9216d63cb958b2a92f18e
junior0803 <junior0803@mail.ru>
2022-03-07 18:18:23 +0300

    BASIC_URL

--- app/src/main/java/com/bts/adamcrm/services/ApiRepository.java
+++ app/src/main/java/com/bts/adamcrm/services/ApiRepository.java
@@ -1,5 +1,7 @@
 package com.bts.adamcrm.services;
 
+import static com.bts.adamcrm.BaseActivity.BASIC_URL;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -22,7 +24,7 @@ public final class ApiRepository {
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.10.11.153:8000/")
+                .baseUrl(BASIC_URL)
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
